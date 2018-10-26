80468a1bc3c2418944f77f7d8146181ba258df04
Görkem Koç <gorkemkoc84@gmail.com>
2018-10-26 10:52:48 +0300

    Web Programlama Altyapıları

---
+++ switch-case.php
@@ -0,0 +1,23 @@
+<?php 
+$not=89;
+switch ($not) {
+	case ($not>=90):
+		echo "A";
+		break;
+	case ($not>=80):
+		echo "B";
+		break;
+	case ($not>=70):
+		echo "C";
+		break;
+	case ($not>=60):
+		echo "D";
+		break;
+	case ($not>=0):
+		echo "E";
+		break;
+	default;
+		echo "yanlış işlem geçersiz";
+		break;
+}
+?>
