bc84fd3e2f5918437c6f62a4af4d69640d795f29
Görkem Koç <gorkemkoc84@gmail.com>
2018-11-01 10:21:34 +0300

    Sayısal Analiz ve Algoritma

---
+++ switch-case.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	int nn;
+	printf("Bir not deðeri giriniz: ");
+	scanf("%d",&nn);
+	if(100>=nn && nn>=0)
+		{
+			switch(nn/10)
+			{
+			case 10: printf("notunuz A+");
+			break;
+			case 9: printf("notunuz A+");
+			break;
+			case 8: printf("notunuz A");
+			break;
+			case 7: printf("notunuz B");
+			break;
+			case 6: printf("notunuz C");
+			break;
+			case 5: printf("notunuz d");
+			break;
+			default:printf("notunuz f");
+			break;
+			}//switch kapanisi
+		}//else kapanisi
+		getchar();
+}
