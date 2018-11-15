9bf3dffe20c92bf535945213a527fbffda9018e4
Görkem Koç <gorkemkoc84@gmail.com>
2018-11-15 13:52:15 +0300

    Web Programlama Altyapıları
    Güz dönemi, vize son sorusu cevabı.

---
+++ vize_son_soru.php
@@ -0,0 +1,15 @@
+<?php
+echo "<table border=1>";
+$ogrenciler = array("Ahamet","Talha","Memet");
+$notlar = array(10,20,30);
+echo("<table border=1>");
+for ($i=0; $i <3 ; $i++) { 
+	echo("<tr>");
+	echo "<td>".$ogrenciler[$i]."</td>";
+	echo "<td>".$notlar[$i]."</td>";
+	echo("</tr>");
+
+}
+echo "</table>";
+
+?>
